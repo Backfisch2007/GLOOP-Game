6e03a45040d11efaef83541ff643cda1fcd5c490
Backfisch2007 <bastian-fischer1303@t-online.de>
2025-02-19 15:08:34 +0100

    save

--- BowlingPin.java
+++ BowlingPin.java
@@ -12,31 +12,64 @@ public class BowlingPin {
         position = pPosition;
         startX = pX;
         startZ = pZ;
-        basis = new GLZylinder(pX, 0, pZ, 2.5, 10); // Unterteil Pin
+        basis = new GLZylinder(pX, 5, pZ, 2.5, 15); // Unterteil Pin
         basis.drehe(90, 90, 0); // Aufrechtstellen 
         basis.setzeFarbe(0, 100, 0); // Farbe Pin
-        
-        oberteil = new GLKugel(pX, 6, pZ, 2.5); // Oberteil Pin
+
+        oberteil = new GLKugel(pX, 11, pZ, 3); // Oberteil Pin
         oberteil.setzeFarbe(0, 100, 0); // Farbe Pin
-        
+
         umgeworfen = false;
         aktuellerWinkel = 0;
     }
 
     // Startet die Umwerf-Animation
-    public void starteUmwerfen(Scoreboard scoreboard) {
-        if (umgeworfen == false) {
+    //public void starteUmwerfen(Scoreboard scoreboard) {
+    //  if (!umgeworfen) {
+    //    scoreboard.erhoeheSpieler1Punkte(); // Erhöhe die Punkte für Spieler 1
+    //  scoreboard.erhoeheSpieler2Punkte(); // Erhöhe die Punkte für Spieler 2
+    //}
+    //umgeworfen = true;
+    //}
+
+    public void starteUmwerfen(Scoreboard scoreboard, double ballX, double ballZ) { //!!!!!!!!!!!!!!!!!!!!!!!!!!
+        if (!umgeworfen) {
             scoreboard.erhoeheSpieler1Punkte(); // Erhöhe die Punkte für Spieler 1
-            scoreboard.erhoeheSpieler2Punkte(); // Erhöhe die Punkte für Spieler 2 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            scoreboard.erhoeheSpieler2Punkte(); // Erhöhe die Punkte für Spieler 2
         }
         umgeworfen = true;
+
+        // Bestimme die Richtung, aus der der Ball kommt
+        double deltaX = ballX - basis.gibX();
+        double deltaZ = ballZ - basis.gibZ();
+
+        // Setze die Drehrichtung basierend auf der Position des Balls
+        if (Math.abs(deltaX) > Math.abs(deltaZ)) {
+            // Ball kommt von links oder rechts
+            if (deltaX > 0) {
+                // Ball kommt von rechts, Pin fällt nach links
+                drehrichtung = -1; // Negative Drehung um die Z-Achse
+            } else {
+                // Ball kommt von links, Pin fällt nach rechts
+                drehrichtung = 1; // Positive Drehung um die Z-Achse
+            }
+        } else {
+            // Ball kommt von vorne oder hinten
+            if (deltaZ > 0) {
+                // Ball kommt von hinten, Pin fällt nach vorne
+                drehrichtung = -1; // Negative Drehung um die X-Achse
+            } else {
+                // Ball kommt von vorne, Pin fällt nach hinten
+                drehrichtung = 1; // Positive Drehung um die X-Achse
+            }
+        }
     }
 
     // Führt die Drehung schrittweise aus
     public void aktualisiere() {
         if (umgeworfen && aktuellerWinkel < 90) {
             // Drehpunkt: Unterer Rand der Basis (Y = 0)
-            double drehpunktY = 0;
+            double drehpunktY = 0; // Drehpunkt auf dem Boden
             // Drehung um die X-Achse am unteren Ende
             basis.drehe(2, 0, 0, basis.gibX(), drehpunktY, basis.gibZ());
             oberteil.drehe(2, 0, 0, basis.gibX(), drehpunktY, basis.gibZ());
@@ -51,8 +84,8 @@ public class BowlingPin {
         oberteil.setzeDrehung(0, 0, 0);
 
         // Zurücksetzen der Position
-        basis.setzePosition(startX, 0, startZ);
-        oberteil.setzePosition(startX, 6, startZ);
+        basis.setzePosition(startX, 5, startZ);
+        oberteil.setzePosition(startX, 11, startZ);
 
         // Zurücksetzen des Status
         umgeworfen = false;
@@ -62,4 +95,16 @@ public class BowlingPin {
     public boolean istUmgeworfen() {
         return umgeworfen;
     }
+
+    // Neue Methode zur Rückgabe der X-Position des Pins
+    public double gibX() {
+        return basis.gibX();
+    }
+
+    // Neue Methode zur Rückgabe der Z-Position des Pins
+    public double gibZ() {
+        return basis.gibZ();
+    }
 }
+
+
--- BowlingSpiel.java
+++ BowlingSpiel.java
@@ -1,4 +1,3 @@
-/**
 import GLOOP.*;
 
 public class BowlingSpiel {
@@ -15,9 +14,9 @@ public class BowlingSpiel {
     private Scoreboard scoreboard; // Scoreboard
 
     // Abmessungen Kollisionsquader
-    private double quaderBreite = 5;  // Breite Quader
-    private double quaderHoehe = 16;  // Höhe Quader
-    private double quaderTiefe = 5;   // Tiefe Quader
+    private double quaderBreite = 5; // Breite Quader
+    private double quaderHoehe = 25; // Höhe Quader
+    private double quaderTiefe = 5; // Tiefe Quader
 
     public BowlingSpiel() {
         himmel = new GLHimmel("rosendal_plains_2.jpg"); // Himmel
@@ -44,151 +43,14 @@ public class BowlingSpiel {
 
         // Array von Quadern für die Kollisionserkennung um alle 10 Pins
         kollisionsQuader = new GLQuader[10];
-        double[] pinXPositionen = {-6.4, 6.4, -19.2, 19.2, 0, -12.8, 12.8, -6.4, 6.4, 0}; // X-Positionen der Pins
-        //double[] pinZPositionen = {185, 185, 178, 178, 178, 171, 171, 171, 171, 164}; // Z-Positionen der Pins 
-        double[] pinZPositionen = {185, 185, 185, 185, 177, 177, 177, 170, 170, 163};
+        double[] pinXPositionen = { -6.4, 6.4, -19.2, 19.2, 0, -12.8, 12.8, -6.4, 6.4, 0 }; // X-Positionen der Pins
+        double[] pinZPositionen = { 185, 185, 185, 185, 175, 175, 175, 165, 165, 155 }; // Z-Position der Pins
 
         pins = new BowlingPin[10]; // Array für alle 10 Pins
         for (int i = 0; i < 10; i++) {
             pins[i] = new BowlingPin(pinXPositionen[i], pinZPositionen[i], i);
-            kollisionsQuader[i] = new GLQuader(pinXPositionen[i], 0, pinZPositionen[i], quaderBreite, quaderHoehe, quaderTiefe);
-            kollisionsQuader[i].setzeSichtbarkeit(false); // Quader unsichtbar machen
-        }
-
-        while (!input.esc()) {
-            steuerung.steuerungsschleife(input);
-
-            // Bewegung der Kugel
-            if (input.istGedrueckt('i')) {
-                ball.bewege(0, 4.5); // Vorwärts
-            }
-            if (input.istGedrueckt('k')) {
-                ball.bewege(0, -4.5); // Rückwärts
-            }
-            if (input.istGedrueckt('j')) {
-                ball.bewege(2.5, 0); // Links
-            }
-            if (input.istGedrueckt('l')) {
-                ball.bewege(-2.5, 0); // Rechts
-            }
-
-            // Kollisionserkennung für alle Pins
-            for (int i = 0; i < 10; i++) {
-                if (kollisionErkannt(ball, kollisionsQuader[i])) {
-                    System.out.println("Kollision mit Pin " + (i + 1) + " erkannt!");
-                    pins[i].starteUmwerfen(scoreboard); // Pin umwerfen
-                    //scoreboard.erhoeheSpieler1Punkte(); // Erhöhe die Punkte für Spieler 1
-                    //scoreboard.erhoeheSpieler2Punkte(); // Erhöhe die Punkte für Spieler 2
-                }
-            }
-
-            // Aktualisiere alle Pins (Animation)
-            for (BowlingPin pin : pins) {
-                pin.aktualisiere();
-            }
-
-            // Pins, Ball, Kamera und Scoreboard zurücksetzen mit der Taste r
-            if (input.istGedrueckt('r')) {
-                for (BowlingPin pin : pins) {
-                    pin.zuruecksetzen(); // Pins zurücksetzen
-                }
-                ball.setzePosition(0, 5, -200); // Ball zurücksetzen
-                steuerung.setzePosition(0, 100, -325); // Kamera zurücksetzen
-                steuerung.setzeBlickpunkt(0, 0, 0); // Blickpunkt zurücksetzen
-                scoreboard.zuruecksetzenPunkte(); // Scoreboard zurücksetzen
-                System.out.println("Alle Pins, Ball, Kamera und Scoreboard wurden zurückgesetzt.");
-                Sys.warte(5000); // Kurze Pause
-            }
-            Sys.warte(10);
-        }
-
-        // Methode zur Kollisionserkennung
-        private boolean kollisionErkannt(Ball ball, GLQuader quader) {
-        // Position und Radius des Balles
-        double ballX = ball.gibX();
-        double ballY = ball.gibY();
-        double ballZ = ball.gibZ();
-        double radius = ball.gibRadius();
-
-        // Position des Quaders 
-        double quaderX = quader.gibX();
-        double quaderY = quader.gibY();
-        double quaderZ = quader.gibZ();
-
-        // Halbe Abmessungen des Quaders
-        double halbeBreite = quaderBreite / 2;
-        double halbeHoehe = quaderHoehe / 2;
-        double halbeTiefe = quaderTiefe / 2;
-
-        // Nächstgelegener Punkt auf dem Quader zur Kugel
-        double naechsterX = Math.max(quaderX - halbeBreite, Math.min(ballX, quaderX + halbeBreite));
-        double naechsterY = Math.max(quaderY - halbeHoehe, Math.min(ballY, quaderY + halbeHoehe));
-        double naechsterZ = Math.max(quaderZ - halbeTiefe, Math.min(ballZ, quaderZ + halbeTiefe));
-
-        // Entfernung zwischen Kugelmittelpunkt und nächstgelegenem Punkt
-        double deltaX = ballX - naechsterX;
-        double deltaY = ballY - naechsterY;
-        double deltaZ = ballZ - naechsterZ;
-        double entfernung = Math.sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
-
-        // Kollision, wenn Entfernung <= Radius
-        return entfernung <= radius;
-    }
-**/
-
-
-import GLOOP.*;
-
-public class BowlingSpiel {
-    private GLKamera kamera;
-    private GLQuader bahn;
-    private GLLicht licht;
-    private GLLicht licht2;
-    private GLHimmel himmel;
-    private GLTastatur input;
-    private KameraSteuerung steuerung;
-    private Ball ball;
-    private GLQuader[] kollisionsQuader; // Array von Quadern für Kollisionserkennung
-    private BowlingPin[] pins; // Array für alle 10 Pins
-    private Scoreboard scoreboard; // Scoreboard
-
-    // Abmessungen Kollisionsquader
-    private double quaderBreite = 5;  // Breite Quader
-    private double quaderHoehe = 25;  // Höhe Quader
-    private double quaderTiefe = 5;   // Tiefe Quader
-
-    public BowlingSpiel() {
-        himmel = new GLHimmel("rosendal_plains_2.jpg"); // Himmel
-        input = new GLTastatur();
-
-        licht = new GLLicht(0, 100, -325); // Lichtquelle 1
-        licht2 = new GLLicht(0, 100, 125); // Lichtquelle 2
-
-        licht.setzeAbschwaechung(0.5); // Lichtquelle 1
-        licht2.setzeAbschwaechung(0.5); // Lichtquelle 2
-
-        ball = new Ball(0, -200); // Ball
-
-        steuerung = new KameraSteuerung(); // Kamera2
-        steuerung.setzePosition(0, 100, 10); // Kamera2
-        steuerung.setzeBlickpunkt(0, 5, 175); // Kamera2
-
-        bahn = new GLQuader(0, -5, 0, 50, 5, 400); // Flacher Quader als Bahn
-        bahn.setzeFarbe(255, 255, 255); // Weiße Farbe damit Textur hell genug
-        bahn.setzeTextur("wood_floor_diff_4k.jpg");
-
-        // Scoreboard erstellen
-        scoreboard = new Scoreboard();
-
-        // Array von Quadern für die Kollisionserkennung um alle 10 Pins
-        kollisionsQuader = new GLQuader[10];
-        double[] pinXPositionen = {-6.4, 6.4, -19.2, 19.2, 0, -12.8, 12.8, -6.4, 6.4, 0}; // X-Positionen der Pins
-        double[] pinZPositionen = {185, 185, 185, 185, 175, 175, 175, 165, 165, 155}; // Z-Position der Pins
-        
-        pins = new BowlingPin[10]; // Array für alle 10 Pins
-        for (int i = 0; i < 10; i++) {
-            pins[i] = new BowlingPin(pinXPositionen[i], pinZPositionen[i], i);
-            kollisionsQuader[i] = new GLQuader(pinXPositionen[i], 0, pinZPositionen[i], quaderBreite, quaderHoehe, quaderTiefe);
+            kollisionsQuader[i] = new GLQuader(pinXPositionen[i], 0, pinZPositionen[i], quaderBreite, quaderHoehe,
+                    quaderTiefe);
             kollisionsQuader[i].setzeSichtbarkeit(false); // Quader unsichtbar machen
         }
 
@@ -259,7 +121,7 @@ public class BowlingSpiel {
         double ballZ = ball.gibZ();
         double radius = ball.gibRadius();
 
-        // Position des Quaders 
+        // Position des Quaders
         double quaderX = quader.gibX();
         double quaderY = quader.gibY();
         double quaderZ = quader.gibZ();
@@ -297,7 +159,8 @@ public class BowlingSpiel {
         double deltaZ = pin1Z - pin2Z;
         double entfernung = Math.sqrt(deltaX * deltaX + deltaZ * deltaZ);
 
-        // Kollision, wenn der Abstand kleiner als ein bestimmter Wert ist (z.B. 5 Einheiten)
+        // Kollision, wenn der Abstand kleiner als ein bestimmter Wert ist (z.B. 5
+        // Einheiten)
         return entfernung < 5;
     }
 }
